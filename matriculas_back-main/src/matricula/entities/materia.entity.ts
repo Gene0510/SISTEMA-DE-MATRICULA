67e1c06496a79bd5e6880e4e25d992dffe4953b1
Gene0510 <gcu.chamba@yavirac.edu.ec>
2024-07-05 03:39:02 -1200

    Se agrega el proyecto

---
+++ matriculas_back-main/src/matricula/entities/materia.entity.ts
@@ -0,0 +1,61 @@
+import { UsuarioEntity } from 'src/usuario/entities/usuario.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { MatriculaEntity } from './matricula.entity';
+
+@Entity('materia', {schema: 'sistema' })
+export class MateriaEntity {
+  @PrimaryGeneratedColumn('increment')
+  id_materia: number;
+
+  @ManyToMany(() => UsuarioEntity, (profesor) => profesor.materia)
+  profesor: UsuarioEntity[];
+
+  @Column('varchar', { length: 50, nullable: false, unique: true })
+  nombre: string;
+
+  @Column('varchar', { length: 50, nullable: true, unique: true })
+  abreviatura: string;
+
+  @Column({
+    name: 'is_active',
+    type: 'boolean',
+    default: true,
+    comment: 'true=activo, false=inactivo',
+  })
+  isActive: boolean;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+    comment: 'Fecha de creacion de la carrera',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+    comment: 'Fecha de actualizacion de la carrera',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamptz',
+    comment: 'Fecha de eliminacion de la carrera',
+  })
+  deletedAt: Date;
+
+  @OneToMany(() => MatriculaEntity, (matricula) => matricula.materia)
+  matricula: MatriculaEntity[];
+}
